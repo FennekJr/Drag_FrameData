dde45fd3b3017549431238adf2f3b669d0914d65
FennekJr <patrickfagard@gmail.com>
2022-10-17 14:42:45 +0200

    Base (Ajout Boostrap/Scss au projet)

---
+++ src/components/header/navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../scss/modules/_Navbar.scss";
+
+class Navbar extends React.Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Navbar;
